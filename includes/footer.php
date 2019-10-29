db2815a44e07053e85796c7b3271c830f0f16cc2
smilardic <stjepan.milardic00@gmail.com>
2019-10-29 20:43:24 +0100

    Added food n stuff

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,14 @@
 <div class="footer1">
-  <p>Stjepan Milardic 2019</p>
-  <a href="admin.php" style="color:white !important;">Admin homepage</a>
+
+  <div class="row">
+    <div class="col-4">
+      GitHub: <a href="https://github.com/yellowdogfunny" class="footerLinks">https://github.com/yellowdogfunny</a>
+    </div>
+    <div class="col-4">
+      <a href="admin.php" style="color:white !important;">Admin homepage</a>
+    </div>
+    <div class="col-4">
+      <p>©Stjepan Milardic - <?php echo date("Y"); ?></p>
+    </div>
+  </div>
 </div>
